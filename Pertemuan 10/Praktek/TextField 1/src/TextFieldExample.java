34df0ae03cb81ecbeab831d49d5e514c6ab45202
arinadwi14 <111201911645@mhs.dinus.ac.id>
2021-05-25 12:29:47 +0700

    Create TextFieldExample.java

---
+++ Pertemuan 10/Praktek/TextField 1/src/TextFieldExample.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+import java.awt.*;
+import java.awt.event.*;
+
+class TextFieldExample {
+    public static void main(String args[]) {
+        Frame f = new Frame("TextField Example");
+        TextField t1,t2;
+        t1 = new TextField("Welcome to Javatpoint.");
+        t1.setBounds(50,100,200,30);
+        t2 = new TextField("AWT Tutorial");
+        t2.setBounds(50,150,200,30);
+        f.add(t1); 
+        f.add(t2);
+        f.setSize(400,400);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.addWindowListener(new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+    }
+    
+}
