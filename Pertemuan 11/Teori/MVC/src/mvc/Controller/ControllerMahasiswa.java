54959170d39659215e28053f02f22d1df1925029
arinadwi14 <111201911645@mhs.dinus.ac.id>
2021-06-01 21:01:29 +0700

    Create ControllerMahasiswa.java

---
+++ Pertemuan 11/Teori/MVC/src/mvc/Controller/ControllerMahasiswa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.Controller;
+
+import mvc.DAO.DAOMahasiswa;
+import mvc.DAOInterface.Imahasiswa;
+import mvc.Model.Mahasiswa;
+import mvc.Model.TableModelMahasiswa;
+import mvc.View.FormMahasiswa;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author User
+ */
+public class ControllerMahasiswa {
+    FormMahasiswa frame;
+    Imahasiswa implMahasiswa;
+    List<Mahasiswa> lb;
+    
+    public void iniField(int row){
+        
+    }
+}
