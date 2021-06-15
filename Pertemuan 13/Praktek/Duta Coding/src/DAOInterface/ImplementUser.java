098f55539539623626e71413d365206e7b4adbd4
arinadwi14 <111201911645@mhs.dinus.ac.id>
2021-06-15 18:53:48 +0700

    Create ImplementUser.java

---
+++ Pertemuan 13/Praktek/Duta Coding/src/DAOInterface/ImplementUser.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAOInterface;
+
+import java.util.List;
+import model.ModUser;
+
+/**
+ *
+ * @author User
+ */
+public interface ImplementUser {
+    
+    public void insert(ModUser mu);
+    public List<ModUser> getAll();
+    public List<ModUser> getCariNama(String nama);
+}
